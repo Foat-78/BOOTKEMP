bc2b3673f6a21f8be6976c528ef3a13b0d80c477
Foat Khisamov <foat.com>
2023-06-28 15:37:59 +1000

    задачи буткемпа

---
+++ Lesson2/Program.cs
@@ -0,0 +1,9 @@
+﻿//3. Линейные O(n) - Это когда алгоритм ваполняет действие n раз
+
+// Напишите программу, которая принимает на вход одно число и возвращает сумму чисел от 1 до n.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!), result = 0;
+for (int i = 1; i <= n; i++)
+    result += i;
+Console.WriteLine($"Сумма число от 1 до {n} = {result}");
